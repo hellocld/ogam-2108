ea8ec4dc3f2bbc98bc04f1c79e5735801c73e462
Christopher Ray Langford <chris@hellocld.com>
2021-08-05 22:30:38 -0400

    Better party management, more enemies in test scene

--- BattleParty.gd
+++ BattleParty.gd
@@ -1,17 +1,19 @@
 class_name BattleParty
 extends Node
 
-export(Array, NodePath) var Members : Array
 
+var members = []
 
 func _ready():
+	for child in get_children():
+		if child is CreatureNode:
+			members.append(child)
 	BattleManager.register_party(self)
 
 
 func get_party_info() -> Dictionary:
 	# provide any "public" info about the members of the party
 	var info = {}
-	for member in Members:
-		var m = get_node(member)
-		info[m] = m.get_info()
+	for member in members:
+		info[member] = member.get_info()
 	return info
